5e5d39fc2611440b6f54b12284338ba07618106b
Mateus <ribeiromateus0042@gmail.com>
2021-08-26 18:16:49 -0300

    line 31 fixed: comparison problem

--- bot.js
+++ bot.js
@@ -16,24 +16,21 @@ client.on("message", async (message) => {
     msg = message.content.split(`${prefix}`);
 
     
-    try{
-        let title = (await axios(`https://api.jikan.moe/v3/search/anime?q=${msg[1]}&page=1`)).data.results
 
-        console.log('***Working**')
+    try{
+        let title = (await axios(`https://api.jikan.moe/v3/search/anime?q=${msg[1]}`)).data.results
+
+        console.log('***Working***')
 
         let list_title_TV = title.filter(obra=>{
           if( obra.type.includes('TV')) return obra
 
         })
 
-
-
-        list_title_TV.map(title=>{
-            if(title.title.includes(`${msg[1]}`)){
-                message.reply(title.image_url)
+        list_title_TV.map(anime=>{
+            if(anime.title.includes(`${msg[1]}`)){
+                message.reply(anime.url);
             }
-            
-
         })
         
     }
